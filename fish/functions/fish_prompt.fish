6255de4ca00979c21c424c4a38e1bfc448eca786
Max Tyrrell <maxtyrrell0@gmail.com>
2019-06-16 16:01:44 +0200

    Set fish greeting to new bonsai and more fish functions

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,10 @@
+function fish_prompt
+  # fire
+  set_color ffec19
+
+  echo -n (prompt_pwd)
+  echo -n " \$ "
+
+  set_color ffffff
+
+end
---
+++ fish/functions/gtp.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gtp --description 'alias gtp=gotop -b'
+	gotop -b $argv;
+end
